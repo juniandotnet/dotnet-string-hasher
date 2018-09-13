42aff2e57793c85ffe1bbcabc077b963f2b79a7c
Junian Triajianto <author@junian.net>
2018-09-14 04:58:15 +0700

    Add basic console application

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace StringHasher
 {
@@ -6,7 +8,39 @@ namespace StringHasher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var algorithms = new Dictionary<string, Func<string, string>> 
+            {
+                {nameof(Hasher.MD5).ToLower(), Hasher.MD5},
+                {nameof(Hasher.SHA1).ToLower(), Hasher.SHA1},
+                {nameof(Hasher.SHA256).ToLower(), Hasher.SHA256},
+                {nameof(Hasher.SHA384).ToLower(), Hasher.SHA384},
+                {nameof(Hasher.SHA512).ToLower(), Hasher.SHA512},
+            };
+
+            if(args.Length < 2)
+            {
+                Console.WriteLine("You need 2 arguments.");
+                return;
+            }
+
+            if(args.Length > 2)
+            {
+                Console.WriteLine("Too many arguments.");
+                return;
+            }
+
+            var algo = args[0].ToLower();
+            var text = args[1];
+
+            if(!algorithms.ContainsKey(algo))
+            {
+                Console.WriteLine($"Algorithm {algo} is unknown.");
+                return;
+            }
+
+            var result = algorithms[algo].Invoke(text);
+
+            Console.WriteLine(result);
         }
     }
 }
